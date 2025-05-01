9f78993b84bde6a83467e26160d4800bdf8ce90b
Dihego Rosa <dihegorc@gmail.com>
2025-05-01 19:07:13 -0300

    Create desafioLogica2.js

---
+++ desafioLogica2.js
@@ -0,0 +1,44 @@
+const nomeHeroi = "MACROHEAD";
+let vitorias = 101;
+let derrotas = 31;
+let rankHeroi;
+
+function calcularRank(vitorias, derrotas) {
+  return vitorias - derrotas;
+}
+
+const saldoDeVitorias = calcularRank(vitorias, derrotas);
+
+switch (true) {
+  case saldoDeVitorias > 0 && saldoDeVitorias <= 10:
+    rankHeroi = "Ferro";
+    break;
+  case saldoDeVitorias > 11 && saldoDeVitorias <= 20:
+    rankHeroi = "Bronze";
+    break;
+  case saldoDeVitorias > 21 && saldoDeVitorias <= 50:
+    rankHeroi = "Prata";
+    break;
+  case saldoDeVitorias > 51 && saldoDeVitorias <= 80:
+    rankHeroi = "Ouro";
+    break;
+  case saldoDeVitorias > 81 && saldoDeVitorias <= 90:
+    rankHeroi = "Diamante";
+    break;
+  case saldoDeVitorias > 91 && saldoDeVitorias <= 100:
+    rankHeroi = "Lendário";
+    break;
+  case saldoDeVitorias >= 101:
+    rankHeroi = "Imortal";
+    break;
+  default:
+    rankHeroi = "Não Ranqueado";
+}
+
+if (rankHeroi === "Não Ranqueado") {
+  console.log(`O Herói ainda não jogou partidas ranqueadas suficientes.`);
+} else {
+  console.log(
+    `Herói tem de saldo de ${saldoDeVitorias} está no ranking ${rankHeroi}`
+  );
+}
